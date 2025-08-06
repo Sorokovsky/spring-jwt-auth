fb4bf94150aae9219f0535b0e54ef12410824caa
Sorokovsky <Sorokovskys@ukr.net>
2025-08-06 22:14:33 +0300

    add bearer to SwaggerConfiguration

--- src/main/java/org/sorokovsky/springsecurityjwtauth/configuration/SwaggerConfiguration.java
+++ src/main/java/org/sorokovsky/springsecurityjwtauth/configuration/SwaggerConfiguration.java
@@ -1,17 +1,26 @@
 package org.sorokovsky.springsecurityjwtauth.configuration;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        name = "Bearer",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
-)
 public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .description("JWT Bearer Token for authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
 }
